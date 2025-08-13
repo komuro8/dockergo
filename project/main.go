10f87707dfd1875b7f33a5fdaaf2a6261b831254
Jheisson Orlando Cabezas Vera <jheissoncabezas@gmail.com>
2025-08-13 10:57:21 -0500

    Update main.go - 13-08

--- project/main.go
+++ project/main.go
@@ -1,12 +1,14 @@
-package main\n\
-import (\n\
-    "net/http"\n\
-    "github.com/gin-gonic/gin"\n\
-)\n\
-func main() {\n\
-    r := gin.Default()\n\
-    r.GET("/", func(c *gin.Context) {\n\
-        c.JSON(http.StatusOK, gin.H{"message": "Hola Mundo desde Go + Gin"})\n\
-    })\n\
-    r.Run(":8080")\n\
+package main
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{"message": "Hola Mundo desde Go + Gin"})
+    })
+    r.Run(":8080")
 }
